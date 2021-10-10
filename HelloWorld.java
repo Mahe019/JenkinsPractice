931054fec864518073c67cbe284bc8b4a21c755a
Mahe019 <92246310+Mahe019@users.noreply.github.com>
2021-10-10 20:17:07 +0530

    Delete HelloWorld.java

--- HelloWorld.java
+++
@@ -1,7 +0,0 @@
-public class HelloWorld {
-public static void main(String[] args) {
-	System.out.println("Hello World...!");
-	System.out.println("Hello Mahe...!");
-	System.out.println("Hello Mahesh...!");
-}
-}
